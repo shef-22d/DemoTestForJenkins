5c881d1a97637013627324593119bdb9292b8afb
Shefali Jain <Shefali.Jain@synapse.com>
2018-05-28 12:01:24 +0530

    First commit in git

---
+++ TestProjectTestNG/src/com/test/sample/demo1.java
@@ -0,0 +1,10 @@
+package com.test.sample;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ demo/src/demo/testdemo.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class testdemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Test case is starting");
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
